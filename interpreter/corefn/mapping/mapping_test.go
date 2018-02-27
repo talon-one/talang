f1d96ce5f0a938cb12fdfd938cb772c7fc54ae83
Tobias Salzmann <salzmann@talon.one>
2018-02-27 12:50:16 +0100

    More mapping tests

--- interpreter/corefn/mapping/mapping_test.go
+++ interpreter/corefn/mapping/mapping_test.go
@@ -3,6 +3,7 @@ package mapping_test
 import (
 	"testing"
 
+	"github.com/ericlagergren/decimal"
 	"github.com/talon-one/talang/block"
 	helpers "github.com/talon-one/talang/testhelpers"
 )
@@ -15,6 +16,22 @@ func TestKV(t *testing.T) {
 			block.NewMap(map[string]*block.Block{
 				"Key1": block.NewString("Value1"),
 				"Key2": block.NewList(block.NewString("Value2"), block.NewString("Value3")),
+			}),
+		},
+		helpers.Test{
+			"kv (Key1 (+ 1 2))",
+			nil,
+			block.NewMap(map[string]*block.Block{
+				"Key1": block.NewDecimal(decimal.New(3, 0)),
+			}),
+		},
+		helpers.Test{
+			"kv (Key1 (kv (SubKey Value)))",
+			nil,
+			block.NewMap(map[string]*block.Block{
+				"Key1": block.NewMap(map[string]*block.Block{
+					"SubKey": block.NewString("Value"),
+				}),
 			}),
 		},
 	)
